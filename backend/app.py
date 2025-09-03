5df332a8958aa6969cc80a713c3cc89f96e544f5
moreniekmeijer <niekjmeijer@gmail.com>
2025-09-03 17:09:33 +0200

    Some minor changes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from datetime import date
 import os
-from random import random
+import random
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'mysecretkey')
 jwt = JWTManager(app)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 db.init_app(app)
 bcrypt.init_app(app)
