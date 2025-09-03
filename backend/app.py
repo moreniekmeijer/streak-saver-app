f425c8613ee2c67eda193be9a0af65551cb5bb52
moreniekmeijer <niekjmeijer@gmail.com>
2025-09-03 17:30:10 +0200

    Updated app_context

--- backend/app.py
+++ backend/app.py
@@ -168,4 +168,5 @@ def update_difficulty():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5500)
+    port = int(os.environ.get("PORT", 5500))
+    app.run(host="0.0.0.0", port=port)
